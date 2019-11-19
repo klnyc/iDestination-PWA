e9677ebeaf9b22ff5016fcfffc2866716aeb5c18
Kenneth Lai <klnyc6@gmail.com>
2019-11-19 13:55:07 -0500

    added nav icons

--- client/nav.js
+++ client/nav.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { MdStar } from 'react-icons/md'
+import { GiFire } from "react-icons/gi"
 
 export default () => {
     return (
         <div id="nav">
-            Navigation Links
+            <div id="navIcon"><GiFire /><div id="navText">Experiences</div></div>
+            <div id="navIcon"><MdStar /><div id="navText">Wishlist</div></div>
         </div>
     )
 }
