1286038a33705b6233797a87cf3b18417df85b8e
Kenneth Lai <klnyc6@gmail.com>
2019-11-19 19:05:19 -0500

    deployed

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     mode: 'development',
     output: {
       path: __dirname,
-      filename: 'bundle.js'
+      filename: './bundle.js'
     },
     resolve: {
         extensions: ['.js', '.jsx']
