4a0c5b48152c6568671d6a18602f4e864aa88628
Kenneth Lai <klnyc6@gmail.com>
2020-08-02 18:27:38 -0400

    before update

--- client/components/login.js
+++ client/components/login.js
@@ -88,3 +88,8 @@ class Login extends React.Component {
 }
 
 export default Login
+
+// iDestination is the Travel journal and travel planner, all in one.
+// Easily view all the places that you've visited in the past 
+// and all the places you wish to visit in the future.
+// Keep track of your adventures and plan your next vacation.
