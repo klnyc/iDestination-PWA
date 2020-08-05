afcff4960e4143387c62f9ef58d312331f3205ea
Kenneth Lai <klnyc6@gmail.com>
2020-08-05 18:40:45 -0400

    added dividers to drawers

--- client/components/drawer.js
+++ client/components/drawer.js
@@ -7,22 +7,36 @@ class Drawer extends React.Component {
         const { user, drawer, category, toggleCategory, home, toggleHome } = this.props
         return (
             <div className={drawer ? "drawer" : "invisible"}>
-                <div className="drawer-name">{user.name}</div>
-                <div className="drawer-email">{user.email}</div>
-                <div className="drawer-section">Display</div>
-                <div className="drawer-link color-link" onClick={() => toggleCategory('all')}>
-                    All{(category.experiences && category.wishlist) ? <span>●</span> : ''}
+                
+                <div className="drawer-section">
+                    <div className="drawer-name">{user.name}</div>
+                    <div className="drawer-email">{user.email}</div>
                 </div>
-                <div className="drawer-link color-link" onClick={() => toggleCategory('experiences')}>
-                    Experiences{(category.experiences && !category.wishlist) ? <span>●</span> : ''}
+
+                <div className="drawer-section">
+                    <div className="drawer-title">Display</div>
+                    <div className="drawer-link color-link" onClick={() => toggleCategory('all')}>
+                        All{(category.experiences && category.wishlist) ? <span>●</span> : ''}
+                    </div>
+                    <div className="drawer-link color-link" onClick={() => toggleCategory('experiences')}>
+                        Experiences{(category.experiences && !category.wishlist) ? <span>●</span> : ''}
+                    </div>
+                    <div className="drawer-link color-link" onClick={() => toggleCategory('wishlist')}>
+                        Wishlist{(!category.experiences && category.wishlist) ? <span>●</span> : ''}
+                    </div>
                 </div>
-                <div className="drawer-link color-link" onClick={() => toggleCategory('wishlist')}>
-                    Wishlist{(!category.experiences && category.wishlist) ? <span>●</span> : ''}
+
+                <div className="drawer-section">
+                    <div className="drawer-title">Home</div>
+                    <div className="drawer-link color-link" onClick={() => toggleHome(home)}>Edit</div>
                 </div>
-                <div className="drawer-section">Home</div>
-                <div className="drawer-link color-link" onClick={() => toggleHome(home)}>Edit</div>
-                <div className="drawer-section color-link" onClick={() => firebase.auth().signOut()}>Sign Out</div>
+
+                <div className="drawer-section signout">
+                    <div className="drawer-title color-link" onClick={() => firebase.auth().signOut()}>Sign Out</div>
+                </div>
+
                 <div className="drawer-copyright">© 2019 iDestination<br/>All Rights Reserved</div>
+
             </div>
         )
     }
