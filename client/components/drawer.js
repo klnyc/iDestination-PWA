5bbff6e7812ecbf7459fd8623535170314b630c3
Kenneth Lai <klnyc6@gmail.com>
2020-08-05 18:50:13 -0400

    took out some empty lines

--- client/components/drawer.js
+++ client/components/drawer.js
@@ -7,12 +7,10 @@ class Drawer extends React.Component {
         const { user, drawer, category, toggleCategory, home, toggleHome } = this.props
         return (
             <div className={drawer ? "drawer" : "invisible"}>
-                
                 <div className="drawer-section">
                     <div className="drawer-name">{user.name}</div>
                     <div className="drawer-email">{user.email}</div>
                 </div>
-
                 <div className="drawer-section">
                     <div className="drawer-title">Display</div>
                     <div className="drawer-link color-link" onClick={() => toggleCategory('all')}>
@@ -25,18 +23,14 @@ class Drawer extends React.Component {
                         Wishlist{(!category.experiences && category.wishlist) ? <span>●</span> : ''}
                     </div>
                 </div>
-
                 <div className="drawer-section">
                     <div className="drawer-title">Home</div>
                     <div className="drawer-link color-link" onClick={() => toggleHome(home)}>Edit</div>
                 </div>
-
                 <div className="drawer-section signout">
                     <div className="drawer-title color-link" onClick={() => firebase.auth().signOut()}>Sign Out</div>
                 </div>
-
                 <div className="drawer-copyright">© 2019 iDestination<br/>All Rights Reserved</div>
-
             </div>
         )
     }
