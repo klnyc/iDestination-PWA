986cd8322b9ab59e4456d98d05174b5a8ca1e278
Kenneth Lai <klnyc6@gmail.com>
2020-08-11 18:52:01 -0400

    added 2nd page

--- client/components/Home.js
+++ client/components/Home.js
@@ -8,11 +8,24 @@ class Home extends React.Component {
         return (
             <div className="home" onClick={() => closeLogIn()}>
                 <div className="home-description">
-                    <div className="home-title">Travel journal. Travel planner. All in one.</div>
-                    <div className="home-text-container">
-                        <div className="home-text">Easily view all the places that you've visited in the past</div>
-                        <div className="home-text">and all the places you wish to visit in the future.</div>
-                        <div className="home-text">Keep track of your adventures and plan your next vacation.</div>
+                    <div className="home-description-content">
+                        <div className="home-description-title">Travel journal. Travel planner. All in one.</div>
+                        <div className="home-description-text">
+                            <div className="home-text">Easily view all the places that you've visited in the past</div>
+                            <div className="home-text">and all the places you wish to visit in the future.</div>
+                            <div className="home-text">Keep track of your adventures and plan your next vacation.</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="home-information">
+                    <div className="home-information-content">
+                        <div className="home-information-text">
+                            <div className="home-information-functionality card"></div>
+                            <div className="home-information-instruction card"></div>
+                        </div>
+                        <div className="home-information-map card">
+
+                        </div>
                     </div>
                 </div>
             </div>
