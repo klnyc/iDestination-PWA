02abd7de9aede4b8a16419aeeec243203e7cd0ba
Kenneth Lai <klnyc6@gmail.com>
2020-08-12 15:02:09 -0400

    finished 2nd page

--- client/components/Home.js
+++ client/components/Home.js
@@ -11,9 +11,9 @@ class Home extends React.Component {
                     <div className="home-description-content">
                         <div className="home-description-title">Travel journal. Travel planner. All in one.</div>
                         <div className="home-description-text-container">
+                            <div className="home-description-text">Keep track of your adventures and plan your next vacation.</div>
                             <div className="home-description-text">Easily view all the places that you've visited in the past</div>
                             <div className="home-description-text">and all the places you wish to visit in the future.</div>
-                            <div className="home-description-text">Keep track of your adventures and plan your next vacation.</div>
                         </div>
                     </div>
                 </div>
@@ -22,15 +22,19 @@ class Home extends React.Component {
                         <div className="home-information-text-container">
                             <div className="home-information-section card">
                                 <div className="home-information-title">Features</div>
-                                <div className="home-information-text">• Search for a location and choose to add it as a past experience or to your wishlist</div>
-                                <div className="home-information-text">• Save your home location</div>
-                                <div className="home-information-text">• Filter the map to show only your past experiences or your future destinations</div>
+                                <div className="home-information-text">• Add locations as past experiences or to your wishlist</div>
+                                <div className="home-information-text">• Add dates to each entry</div>
+                                <div className="home-information-text">• Filter the map by categories</div>
+                                <div className="home-information-text">• Set your home location</div>
+                                <div className="home-information-text">• View all locations in a list</div>
                             </div>
                             <div className="home-information-section card">
-                            <div className="home-information-title">How to add the app to your phone</div>
-                                <div className="home-information-text">• Search for a location and choose to add it as a past experience or to your wishlist</div>
-                                <div className="home-information-text">• Save your home location</div>
-                                <div className="home-information-text">• Filter the map to show only your past experiences or your future destinations</div>
+                            <div className="home-information-title">Optimized for iPhone & Desktop</div>
+                                <div className="home-information-text">• To add this app to your iPhone:</div>
+                                <div className="home-information-text">• Visit idestination.web.app</div>
+                                <div className="home-information-text">• Go to options, settings, or preferences</div>
+                                <div className="home-information-text">• Click "Add to Home Screen"</div>
+                                <div className="home-information-text">• For any questions, please email klnyc6@gmail.com</div>
                             </div>
                         </div>
                         <div className="home-information-map card"><img src="img/screenshot.png"></img></div>
