7325fd8e979e537467afb8ea46760a6de5307c1d
Kenneth Lai <klnyc6@gmail.com>
2020-09-23 16:26:32 -0400

    changed to grid

--- client/components/Home.js
+++ client/components/Home.js
@@ -7,21 +7,23 @@ class Home extends React.Component {
         const { closeLogIn } = this.props
         return (
             <div className="home" onClick={() => closeLogIn()}>
+
                 <div className="home-description">
                     <div className="home-description-content">
-                        <div className="home-description-title">
+                        <div className="home-description-slogan">
                             <span>Travel journal.</span><span>Travel planner.</span><span>All in one.</span>
                         </div>
                         <div className="home-description-text-container">
                             <div className="home-description-text">Keep track of your adventures.</div>
                             <div className="home-description-text">Plan your next vacation.</div>
                         </div>
+                        <div className="home-description-map"><img src="img/screenshot.png" className="card"></img></div>
                     </div>
                 </div>
+
                 <div className="home-information">
                     <div className="home-information-content">
-                        <div className="home-information-map"><img src="img/screenshot.png" className="card"></img></div>
-                        <div className="home-information-text-container">
+                        <div className="home-information-row top">
                             <div className="home-information-section card">
                                 <div className="home-information-title">Features</div>
                                 <div className="home-information-text">• Add locations as past experiences or to your wishlist</div>
@@ -31,7 +33,26 @@ class Home extends React.Component {
                                 <div className="home-information-text">• View all locations in a list</div>
                             </div>
                             <div className="home-information-section card">
-                            <div className="home-information-title">Optimized for iPhone & Desktop</div>
+                                <div className="home-information-title">Optimized for iPhone & Desktop</div>
+                                <div className="home-information-text">• To add this app to your iPhone:</div>
+                                <div className="home-information-text">• Visit idestination.web.app</div>
+                                <div className="home-information-text">• Go to your browser options, settings, or preferences</div>
+                                <div className="home-information-text">• Click "Add to Home Screen"</div>
+                                <div className="home-information-text">• Open iDestination from your home screen</div>
+                                <div className="home-information-text">• For any questions, please email klnyc6@gmail.com</div>
+                            </div>
+                        </div>
+                        <div className="home-information-row bottom">
+                            <div className="home-information-section card">
+                                <div className="home-information-title">Features</div>
+                                <div className="home-information-text">• Add locations as past experiences or to your wishlist</div>
+                                <div className="home-information-text">• Add dates to each entry</div>
+                                <div className="home-information-text">• Filter the map by categories</div>
+                                <div className="home-information-text">• Set your home location</div>
+                                <div className="home-information-text">• View all locations in a list</div>
+                            </div>
+                            <div className="home-information-section card">
+                                <div className="home-information-title">Optimized for iPhone & Desktop</div>
                                 <div className="home-information-text">• To add this app to your iPhone:</div>
                                 <div className="home-information-text">• Visit idestination.web.app</div>
                                 <div className="home-information-text">• Go to your browser options, settings, or preferences</div>
@@ -42,6 +63,7 @@ class Home extends React.Component {
                         </div>
                     </div>
                 </div>
+
             </div>
         )
     }
